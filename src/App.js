42ceefd7259c507dd421a99642c2b2205a1778d5
iAdityarajpurohit <108542528+iAdityarajpurohit@users.noreply.github.com>
2022-11-12 23:29:42 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import Resturant from "./comonent/Basic/Resturant";
+// import UseState from './comonent/Hooks/useState';
+// import UseEffect from './comonent/Hooks/useEffect';
+// import UseReducer from './comonent/Hooks/useReducer';
+
+// import Todo from './comonent/Todo-react/todo';
+// import Temperatur from './comonent/weather/temperatur';
+// import Temp from './comonent/weather/temperatur';
+// import ColorPicker from './comonent/weather/colorPicker';
+
+const App = () => {
+
+
+  
+  return (
+    
+    <>
+        {/* { < ColorPicker /> } */}
+          {/* { < Temp />} */}
+
+      {/* {< Todo />} */}
+       
+
+     {/* {< UseReducer />} */}
+    {/* {< UseEffect />} */}
+    {/* {< UseState />} */}
+
+     {<Resturant/>}
+    </>
+  )
+};
+export default App;
